6e5d7babfde3f9e52467d1198f7d840d0f8462c6
TheL1ne <the_l1ne@gmx.de>
2021-02-15 00:04:03 +0100

    add loading of ontology

--- java/detektor/src/main/java/detektor/Main.java
+++ java/detektor/src/main/java/detektor/Main.java
@@ -5,19 +5,22 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
+import openllet.owlapi.OpenlletReasoner;
+import openllet.owlapi.OpenlletReasonerFactory;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 		IRI file = IRI.create("C:\\Users\\The L!ne\\go\\src\\github.com\\TheL1ne\\Sensor_Aktor_controller\\istZustand.owl");
-		System.out.println("ontology to load: " + file.toString());
 		
 		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
-		/*
+		
 		// get openllet reasoner
 		final OpenlletReasoner reasoner = OpenlletReasonerFactory.getInstance().createReasoner(ontology);		
 		reasoner.getKB().realize();
-		reasoner.getKB().printClassTree();*/
+		reasoner.getKB().printClassTree();
+		System.out.println(ontology.getAxiomCount());
 	}
 
 }
