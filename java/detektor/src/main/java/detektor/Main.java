33e6fa7614be00782dd3e1cd0cf4f8e27101da89
TheL1ne <the_l1ne@gmx.de>
2021-02-15 01:09:23 +0100

    loading of ontology works

--- java/detektor/src/main/java/detektor/Main.java
+++ java/detektor/src/main/java/detektor/Main.java
@@ -1,7 +1,8 @@
 package detektor;
 
+import java.io.File;
+
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
@@ -12,14 +13,13 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		IRI file = IRI.create("C:\\Users\\The L!ne\\go\\src\\github.com\\TheL1ne\\Sensor_Aktor_controller\\istZustand.owl");
+		File file = new File("C:\\Users\\The L!ne\\go\\src\\github.com\\TheL1ne\\Sensor_Aktor_controller\\istZustand.owl");
 		
 		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
 		
 		// get openllet reasoner
 		final OpenlletReasoner reasoner = OpenlletReasonerFactory.getInstance().createReasoner(ontology);		
 		reasoner.getKB().realize();
-		reasoner.getKB().printClassTree();
 		System.out.println(ontology.getAxiomCount());
 	}
 
